48b126046129ca6c8093c7d537d9d2ca8157e3b1
SimonWJohnson <simonwjohnson@gmail.com>
2026-08-17 13:00:39 +0930

    Set up initial Spring Boot project structure

---
+++ src/main/java/com/example/demo/Comp3011Assignment01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp3011Assignment01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp3011Assignment01Application.class, args);
+	}
+
+}
