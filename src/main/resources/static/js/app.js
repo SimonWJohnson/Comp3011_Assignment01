0476321cf7407bc0312b85a945304119ec99b21d
SimonWJohnson <simonwjohnson@gmail.com>
2026-08-26 16:52:39 +0930

    Add js stop recording functionality

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -4,9 +4,18 @@ const startButton = document.getElementById("start-recording");
 const stopButton = document.getElementById("stop-recording");
 const recordingStatus = document.getElementById("recording-status");
 
-// Event listeners
+// ## Event listeners ##
+
+// Start recording
 startButton.addEventListener("click", function(){
 	startButton.disabled = true;
 	stopButton.disabled = false;
 	recordingStatus.textContent = "Status: Recording...";
+});
+
+// Stop recording
+stopButton.addEventListener("click", function(){
+	startButton.disabled = false;
+	stopButton.disabled = true;
+	recordingStatus.textContent = "Status: Ready";
 });
