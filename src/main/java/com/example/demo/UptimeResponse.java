627617aee99ed135966c1779631357dad162928b
SimonWJohnson <simonwjohnson@gmail.com>
2026-09-07 15:38:42 +0930

    Add first pass logic for UptimeResponse

--- src/main/java/com/example/demo/UptimeResponse.java
+++ src/main/java/com/example/demo/UptimeResponse.java
@@ -2,4 +2,30 @@ package com.example.demo;
 
 public class UptimeResponse {
 
+
+	private String utcServerStart; // RFC 3339 date-time string
+	private String utcNow; // RFC 3339 date-time string
+	private double serverUptimeSeconds; // non-negative floating point number
+	
+	// Constructor
+	public UptimeResponse(String utcServerStart, String utcNow, double serverUptimeSeconds) {
+		this.utcServerStart = utcServerStart;
+		this.utcNow = utcNow;
+		this.serverUptimeSeconds = serverUptimeSeconds;
+	}
+	
+	
+	// This is the JSON shape represented as aJava object
+	public String getUtcServerStart(){
+		return utcServerStart;
+	}
+	
+	public String getUtcNow() {
+		return utcNow;
+	}
+	
+	public double getServerUptimeSeconds() {
+		return serverUptimeSeconds;
+	}
+	
 }
