735fe9390ae53a5603f3dd9cdd6ec2b761d34dae
SimonWJohnson <simonwjohnson@gmail.com>
2026-09-07 11:56:05 +0930

    Add microphone access for audio recording

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -6,10 +6,20 @@ const recordingStatus = document.getElementById("recording-status");
 const statusIndicator = document.getElementById("status-indicator");
 const statusText = document.getElementById("status-text");
 
+// Microphone access
+let mediaStream = null;
+
+// Audio data
+let mediaRecorder = nul;
+let audioChunks = [];
+
 // ## Event listeners, add/remove a state classification from this element ##
 
 // Start recording
-startButton.addEventListener("click", function(){
+startButton.addEventListener("click", async function(){ 
+	// requesting microphone access is asynchronous - the browser may need to permission-prompt the user and wait for a response
+	mediaStream = await navigator.mediaDevices.getUserMedia({audio: true});
+	
 	startButton.disabled = true;
 	stopButton.disabled = false;
 	//recordingStatus.textContent = "Status: Recording...";
