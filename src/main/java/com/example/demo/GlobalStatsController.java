d323fe12392aec6cefb4231784c86fe3d1c020c2
SimonWJohnson <simonwjohnson@gmail.com>
2026-09-11 17:55:00 +0930

    Add first pass logic to GlobalStatsController

--- src/main/java/com/example/demo/GlobalStatsController.java
+++ src/main/java/com/example/demo/GlobalStatsController.java
@@ -1,5 +1,26 @@
 package com.example.demo;
 
-public class GlobalStatsController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/v1/global")
+public class GlobalStatsController {
+	
+	// Enable controller to access GlobalStatsService via dependency injection
+	private final GlobalStatsService statsService;
+	
+	// Constructor
+	public GlobalStatsController(GlobalStatsService statsService) {
+		this.statsService = statsService;
+	}
+	
+	@GetMapping("/stats")
+	public GlobalStatsResponse getGlobalStats() {
+		return new GlobalStatsResponse(
+				statsService.getInputTokens(), 
+				statsService.getOutputTokens()
+				);
+	};
 }
