ec487b332069ab1ee314f4311e42d8a194f1ff47
SimonWJohnson <simonwjohnson@gmail.com>
2026-09-11 17:42:08 +0930

    Add first pass logic to GlobalStatsResponse

--- src/main/java/com/example/demo/GlobalStatsResponse.java
+++ src/main/java/com/example/demo/GlobalStatsResponse.java
@@ -1,5 +1,23 @@
 package com.example.demo;
 
 public class GlobalStatsResponse {
+	
+	private long inputTokens;
+	private long outputTokens;
 
+	
+	// Constructor
+	public GlobalStatsResponse(long inputTokens, long outputTokens) {
+		this.inputTokens = inputTokens;
+		this.outputTokens = outputTokens;
+	}
+	
+	// Getters
+	public long getInputTokens() {
+		return inputTokens;
+	}
+	
+	public long getOutputTokens() {
+		return outputTokens;
+	}
 }
