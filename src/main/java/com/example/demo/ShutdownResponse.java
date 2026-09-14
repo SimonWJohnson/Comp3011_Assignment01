9e0739f4cb29f82c0e3059461380b8503e677da0
SimonWJohnson <simonwjohnson@gmail.com>
2026-09-14 16:17:01 +0930

    Add first pass logic to SutdownResponse

--- src/main/java/com/example/demo/ShutdownResponse.java
+++ src/main/java/com/example/demo/ShutdownResponse.java
@@ -2,4 +2,15 @@ package com.example.demo;
 
 public class ShutdownResponse {
 
+	private String message;
+	
+	// Constructor
+	public ShutdownResponse(String message) {
+		this.message = message;
+	}
+	
+	// Getter
+	public String getMessage() {
+		return message;
+	}
 }
