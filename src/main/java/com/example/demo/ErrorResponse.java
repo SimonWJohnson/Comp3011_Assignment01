740aa62a83e6a73ed9c23f605c0e4f9eb6388471
SimonWJohnson <simonwjohnson@gmail.com>
2026-09-15 17:49:42 +0930

    Update ErrorResponse to match API contract

--- src/main/java/com/example/demo/ErrorResponse.java
+++ src/main/java/com/example/demo/ErrorResponse.java
@@ -2,18 +2,49 @@ package com.example.demo;
 
 public class ErrorResponse {
 
-	// Store the error message returned to the client
+	// UTC timestamp showing when the error occurred
+	private String timestamp;
+	
+	// HTTP status code asscociated with the error
+	private int status;
+	
+	// Standard HTTP error description
 	private String error;
 	
+	// Human-readable description of the specific error
+	private String message;
+	
+	// API endpoint at which the error occurred
+	private String path;
+	
 	// Constructor
-	// Create the ErrorResponse containing the supplied error message
-	public ErrorResponse(String error) {
+	// Create the ErrorResponse containing all required info to describe an unsuccessful API request
+	public ErrorResponse(String timestamp, int status, String error, String message, String path) {
+		this.timestamp = timestamp;
+		this.status = status;
 		this.error = error;
+		this.message = message;
+		this.path = path;
 	}
 	
-	// Getter
-	// Serialise the object into JSON
+	// Getters
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
 	public String getError() {
 		return error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getPath() {
+		return path;
 	}
 }
