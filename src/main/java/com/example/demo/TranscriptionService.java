866085297d05d731cfd8860fc35dbca7701b32cc
SimonWJohnson <simonwjohnson@gmail.com>
2026-09-15 21:47:41 +0930

    Create TranscriptionService

---
+++ src/main/java/com/example/demo/TranscriptionService.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+
+// Handles communication between the Spring backend and the external speech-to-text CLoud service
+@Service
+public class TranscriptionService {
+
+}
