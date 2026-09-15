72ddfafdf180d1e7e6b39e74115dcd70e9e0acb9
SimonWJohnson <simonwjohnson@gmail.com>
2026-09-15 16:58:37 +0930

    Add first pass logic for ErrorResponse class

--- src/main/java/com/example/demo/ErrorResponse.java
+++ src/main/java/com/example/demo/ErrorResponse.java
@@ -2,4 +2,17 @@ package com.example.demo;
 
 public class ErrorResponse {
 
+	// Store the error message returned to the client
+	private String error;
+	
+	// Constructor
+	// Create the ErrorResponse containing the supplied error message
+	public ErrorResponse(String error) {
+		this.error = error;
+	}
+	
+	// Getter
+	public String getError() {
+		return error;
+	}
 }
